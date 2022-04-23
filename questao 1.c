2282ed16417abb659d9939062ed357e3973c886b
Keven-Lucas <103372443+Keven-Lucas@users.noreply.github.com>
2022-04-23 14:03:17 -0300

    Create questao 1.c

---
+++ questao 1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+
+    int dia;
+
+    do{
+        printf("Informe um numero de (1 ate 7):\n ");
+        scanf("%d", &dia);
+
+        if(dia == 0){
+            break;
+        }else
+        if(dia > 7){
+            printf("Numero invalido!\n");
+        }else
+        if (dia == 1){
+            printf("Domingo!\n");
+        
+        }else
+        if(dia == 2){
+            printf("Segunda-feira!\n");
+            
+        }else
+        if(dia == 3){
+            printf("Terca-feira!\n");
+            
+        }else
+        if(dia == 4){
+            printf("Quarta-feira!\n");
+            
+        }else
+        if(dia == 5){
+            printf("Quinta-feira!\n");
+            
+        }else
+        if(dia == 6){
+            printf("Sexta-feira!\n");
+            
+        }else
+        if(dia == 7){
+            printf("Sabado!\n");
+            
+        }
+
+    }while(dia != 0);
+
+    return 0;
+    }
