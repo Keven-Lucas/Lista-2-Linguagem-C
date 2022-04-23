9b71caca0b1759cf3c8a9a669275d85c9346b461
Keven-Lucas <103372443+Keven-Lucas@users.noreply.github.com>
2022-04-23 14:09:29 -0300

    Create questao 9.c

---
+++ questao 9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int num,f1=0,f2=1,f3;
+    scanf("%d",&num);
+
+    if (num < 0){
+        printf("Numero invalido!");
+    }else
+        printf("0 - 1");
+
+        while(f2 < num){
+            f3 = f2 + f1;
+            printf(" %d",f3);
+            f1=f2;
+            f2=f3;
+        }
+        printf("\n");
+        system("pause");
+
+return 0;
+}
